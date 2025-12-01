a399bb2913cd2873ede6f199107de1d855c18f49
Alireza-Hamidi2001 <alireza.hamidi.eng@gmail.com>
2025-12-01 22:41:21 +0330

    change the footer styles

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,14 +1,27 @@
 import styled from 'styled-components';
+import { respond } from '../utils/mixins';
 
 function Footer() {
     const FooterStyled = styled.footer`
         margin-top: auto;
+
+        ${respond('tab-port')`
+            padding: 1.5rem
+        `}
     `;
 
     const CopyrightText2 = styled.p`
         font-family: 'KottaOne';
         font-size: 1.4rem;
         font-weight: 400;
+
+        ${respond('tab-port')`
+            font-size: 1.2rem;
+        `}
+
+        ${respond('phone')`
+            font-size: 1rem;
+        `}
     `;
     const CopyrightText1 = styled.p`
         font-family: 'Caveat';
@@ -24,7 +37,9 @@ function Footer() {
         <FooterStyled>
             <Copyright>&copy; 2025</Copyright>
             <CopyrightText1> Alireza Hamidi</CopyrightText1>
-            <CopyrightText2>Based on a design of Jonas</CopyrightText2>
+            <CopyrightText2>
+                Based on a design of Jonas
+            </CopyrightText2>
         </FooterStyled>
     );
 }
